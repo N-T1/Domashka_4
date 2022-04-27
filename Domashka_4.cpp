4f7e6c66ccd37149758c4f105b4ac96914b1b10f
N-T1 <nastya.1227@yandex.ru>
2022-04-27 19:09:23 +0300

    4

---
+++ Domashka_4.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	double a = 4.6;
+	double b = 1.5;
+	double y, t;
+	y = a + log(b * b);
+	if (y == b)
+	{
+		t = (2 * y + sqrt(y * y - a)) / (2 * b - sqrt(a * a - y));
+	}
+	if (y < b)
+	{
+		t = sin(y) * sin(y) + 1 / tan(a - b);
+	}
+	if (y > b)
+	{
+		t = pow(y * sin(a), 1 / 3) + 1 / sqrt(y * cos(b));
+	}
+	cout << "a = " << a << endl;
+	cout << "b = " << b << endl;
+	cout << "y = " << y << endl;
+	cout << "t = " << t << endl;
+}
